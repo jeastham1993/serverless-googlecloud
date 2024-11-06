a12b0a94814ee6f29fba1e4eb7bcd1d401f8fb17
james.eastham <james.eastham@datadoghq.com>
2024-11-06 16:51:22 +0000

    Update CI deployment

--- src/backend/infra/variables.tf
+++ src/backend/infra/variables.tf
@@ -8,17 +8,21 @@ variable "live_image_tag" {
 
 variable "canary_image_tag" {
     type = string
+    default = ""
 }
 
 variable "canary_enabled" {
   description = "Enable the canary"
   type = bool
+  default = false
 }
 
 variable "canary_percent" {
   description = "Percentage of traffic to send to the canary"
   type = number
+  default = 10
 }
+
 variable "force_new_revision" {
   type = bool
   default = false
